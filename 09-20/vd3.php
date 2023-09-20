9308aef9070392a01c30702557a4c57022aa8e5e
Krist <phanthithuthao0204@gmail.com>
2023-09-20 11:42:49 +0700

    Upload vd3

---
+++ 09-20/vd3.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!--table>tr*10>(td+{7})+(td+{$})+(td) -->
+    <?php $n=5; ?>
+    <table border="1">
+        <?php 
+            for ($i=1; $i <= 10; $i++) 
+            {
+                ?>
+                <tr>
+                    <td><?php echo $n; ?></td>
+                    <td><?php echo $i; ?></td>
+                    <td>=<?php echo $n*$i; ?></td>
+                </tr>
+                <?php
+            }
+        ?>
+    </table>
+</body>
+</html>
